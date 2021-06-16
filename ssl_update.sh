047da0101382fca2270ae5fa59bd4af948d571fa
孤爺仔 <guyezi@users.noreply.github.com>
2021-06-17 02:08:58 +0800

    Update ssl_update.sh

--- ssl_update.sh
+++ ssl_update.sh
@@ -8,6 +8,6 @@ domain=$(grep '\"add\"' $v2ray_qr_config_file | awk -F '"' '{print $4}')
 systemctl stop nginx &> /dev/null
 sleep 1
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /dev/null
-"/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc
+"/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath /cert/"${domain}".crt --keypath /cert/"${domain}".key --ecc
 sleep 1
 systemctl start nginx &> /dev/null
